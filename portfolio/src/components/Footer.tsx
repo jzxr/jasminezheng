8d4975dbb0050f58eb29858f48f912f3c1c57f39
jasmine zheng <jasminezxr@gmail.com>
2024-05-14 14:28:07 +0800

    Initial commit

---
+++ portfolio/src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <p>Footer content</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
+// Add this line at the end of the file
+export {};
---
+++ portfolio/src/components/Projects.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+      {/* Add your project details here */}
+    </section>
+  );
+};
+
+export default Projects;
+export {};
